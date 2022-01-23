a5df9165d41ab7f8023c75e83cc052e0fb753e6e
Alisevichandrew <dopysk@yandex.ru>
2022-01-23 14:27:20 +0300

    Signature Verified as a result

--- app.py
+++ app.py
@@ -1,65 +1,80 @@
-from functools import wraps
-from flask import Flask, request, jsonify, make_response, render_template, session
+from flask import Flask, request, jsonify, make_response, request, render_template, session
 import jwt
 from datetime import datetime, timedelta
+from functools import wraps
 
-# app = Flask(__name__)
-# app.config['SECRET_kEY'] = 'b'SO\x07\xfd\x7fDh\xc4`\x16\xa6\xbd\xd7T\xe1&'
+
 app = Flask(__name__)
-app.secret_key = b'Z\x864\x94\x8a\xf2\x92\x1c\xb1&\xda\xff\x84\xdfc\x8c'
-#generation by command in terminal:> python -c 'import os; print(os.urandom(16))'
 
+
+app.config['SECRET_KEY'] = b'h\xd9=n\xab\xa4i\x81)?\xd2\x1ev\x10\xf9\x10'
 
 def token_required(func):
+    # decorator factory which invoks update_wrapper() method and passes decorated function as an argument
     @wraps(func)
     def decorated(*args, **kwargs):
         token = request.args.get('token')
         if not token:
             return jsonify({'Alert!': 'Token is missing!'}), 401
-        
+
         try:
-            # why 'data'?
-            token = jwt.decode(token, app.config['SECRET KEY'])
+
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        # You can use the JWT errors in exception
+        # except jwt.InvalidTokenError:
+        #     return 'Invalid token. Please log in again.'
         except:
-            return jsonify({'Message': 'Invalid Token'}), 403
-        
+            return jsonify({'Message': 'Invalid token'}), 403
         return func(*args, **kwargs)
     return decorated
-    
 
-#Home
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return 'Logged in currently!'
+        return 'logged in currently'
 
-#Public
 @app.route('/public')
 def public():
     return 'For Public'
 
-#Authenticated
+# auth only if you copy your token and paste it after /auth?query=XXXXXYour TokenXXXXX
+# Hit enter and you will get the message below.
+
+
 @app.route('/auth')
 @token_required
 def auth():
-    return 'JWT is verified. Welcome to you dashboard!' 
+    return 'JWT is verified. Welcome to your dashboard !  '
 
-# Login
+# Login page
+
+
 @app.route('/login', methods=['POST'])
 def login():
     if request.form['username'] and request.form['password'] == '123456':
         session['logged_in'] = True
+
         token = jwt.encode({
             'user': request.form['username'],
+            
             'expiration': str(datetime.utcnow() + timedelta(seconds=60))
         },
             app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('utf-8')})
-
+        return jsonify({'token': token})
+        
     else:
-        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed!"'})
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'})
+
+
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    pass
+# your code goes here
 
 
 if __name__ == "__main__":
